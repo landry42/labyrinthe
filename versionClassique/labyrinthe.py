29e8e758bf9951846e9f2fc675b2f8417ce00dde
Landry <landry.valetoux.lv@gmail.com>
2020-01-13 15:43:21 +0100

    Auto stash before merge of "Nathan" and "origin/Nathan"

--- versionClassique/labyrinthe.py
+++ versionClassique/labyrinthe.py
@@ -8,6 +8,10 @@
    
    Ce module gère la liste des joueurs. 
 """
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
 import random
 from joueur import *
 
@@ -69,10 +73,75 @@ def changerJoueurCourant(joueurs):
     passe au joueur suivant (change le joueur courant donc)
     paramètres: joueurs la liste des joueurs
     cette fonction ne retourne rien mais modifie la liste des joueurs
+<<<<<<< Updated upstream
+=======
+=======
+
+from listeJoueurs import *
+from plateau import *
+
+
+def Labyrinthe(nomsJoueurs=["joueur1","joueurs2"],nbTresors=24, nbTresorsMax=0):
+    """
+    permet de créer un labyrinthe avec nbJoueurs joueurs, nbTresors trésors
+    chacun des joueurs aura au plus nbTresorMax à trouver
+    si ce dernier paramètre est à 0, on distribuera le maximum de trésors possible 
+    à chaque joueur en restant équitable
+    un joueur courant est choisi et la phase est initialisée
+    paramètres: nomsJoueurs est la liste des noms des joueurs participant à la partie (entre 1 et 4)
+                nbTresors le nombre de trésors différents il en faut au moins 12 et au plus 49
+                nbTresorMax le nombre de trésors maximum distribué à chaque joueur
+    résultat: le labyrinthe crée
+    """
+    if nbTresorsMax == 0:
+        nbTresorsMax = nbTresors // len(nomsJoueurs)
+
+    return {"nomsJoueurs":nomsJoueurs,"nbTresors":nbTresors,"nbTresorsMax":nbTresorsMax}
+
+def getPlateau(labyrinthe):
+    """
+    retourne la matrice représentant le plateau de jeu
+    paramètre: labyrinthe le labyrinthe considéré
+    résultat: la matrice représentant le plateau de ce labyrinthe
+    """
+    pass
+
+def getNbParticipants(labyrinthe):
+    """
+    retourne le nombre de joueurs engagés dans la partie
+    paramètre: labyrinthe le labyrinthe considéré
+    résultat: le nombre de joueurs de la partie
+    """
+    pass
+
+def getNomJoueurCourant(labyrinthe):
+    """
+    retourne le nom du joueur courant
+    paramètre: labyrinthe le labyrinthe considéré
+    résultat: le nom du joueurs courant
+    """
+    pass
+
+def getNumJoueurCourant(labyrinthe):
+    """
+    retourne le numero du joueur courant
+    paramètre: labyrinthe le labyrinthe considéré
+    résultat: le numero du joueurs courant
+    """
+    pass
+
+def getPhase(labyrinthe):
+    """
+    retourne la phase du jeu courante
+    paramètre: labyrinthe le labyrinthe considéré
+    résultat: le numéro de la phase de jeu courante
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
     """   
     pass
 
 def getNbJoueurs(joueurs):
+<<<<<<< Updated upstream
     """
     retourne le nombre de joueurs participant à la partie
     paramètre: joueurs la liste des joueurs
@@ -88,6 +157,68 @@ def getJoueurCourant(joueurs):
     """
     return joueurs[0]
 
+def joueurCourantTrouveTresor(joueurs):
+    """
+    Met à jour le joueur courant lorsqu'il a trouvé un trésor
+    c-à-d enlève le trésor de sa liste de trésors à trouver
+    paramètre: joueurs la liste des joueurs
+    cette fonction ne retourne rien mais modifie la liste des joueurs
+=======
+    """
+    retourne le nombre de joueurs participant à la partie
+    paramètre: joueurs la liste des joueurs
+    résultat: le nombre de joueurs de la partie
+>>>>>>> Stashed changes
+    """
+    return len(joueurs)
+
+<<<<<<< Updated upstream
+def nbTresorsRestantsJoueur(joueurs,numJoueur):
+    """
+    retourne le nombre de trésors restant pour le joueur dont le numéro 
+    est donné en paramètre
+    paramètres: joueurs la liste des joueurs
+                numJoueur le numéro du joueur
+    résultat: le nombre de trésors que joueur numJoueur doit encore trouver
+=======
+def getJoueurCourant(joueurs):
+    """
+    retourne le joueur courant
+    paramètre: joueurs la liste des joueurs
+    résultat: le joueur courant
+>>>>>>> Stashed changes
+    """
+    return joueurs[0]
+
+<<<<<<< Updated upstream
+def numJoueurCourant(joueurs):
+    """
+    retourne le numéro du joueur courant
+    paramètre: joueurs la liste des joueurs
+    résultat: le numéro du joueur courant
+    """
+    pass
+
+def nomJoueurCourant(joueurs):
+    """
+    retourne le nom du joueur courant
+    paramètre: joueurs la liste des joueurs
+    résultat: le nom du joueur courant
+    """
+    pass
+
+def nomJoueur(joueurs,numJoueur):
+    """
+    retourne le nom du joueur dont le numero est donné en paramètre
+    paramètres: joueurs la liste des joueurs
+                numJoueur le numéro du joueur    
+    résultat: le nom du joueur numJoueur
+    """
+    pass
+
+def prochainTresorJoueur(joueurs,numJoueur):
+    """
+=======
 def joueurCourantTrouveTresor(joueurs):
     """
     Met à jour le joueur courant lorsqu'il a trouvé un trésor
@@ -134,6 +265,7 @@ def nomJoueur(joueurs,numJoueur):
 
 def prochainTresorJoueur(joueurs,numJoueur):
     """
+>>>>>>> Stashed changes
     retourne le trésor courant du joueur dont le numero est donné en paramètre
     paramètres: joueurs la liste des joueurs
                 numJoueur le numéro du joueur    
